f6294565e6ee53da2866a95d82b6f578930d189a
sanhan-debug <snhanayilov@gmail.com>
2024-12-29 02:01:29 +0400

    books page and product component

--- src/pages/User/Books/index.jsx
+++ src/pages/User/Books/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import { useContext } from 'react'
+import { productContext } from '../../../context/ProductContext'
+import Product from '../../../components/User/Product'
 function Books() {
+
+  let { products } = useContext(productContext)
+
   return (
-    <div>Books</div>
-  )
+    < Product  products={products} />
+  )    
 }
 
 export default Books
